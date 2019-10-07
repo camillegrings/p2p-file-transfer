0b725f0cad1b3d4180c34113bd8db62d649684bb
Camille Grings Silva <camille.silva@cwi.com.br>
2019-10-06 21:04:05 -0300

    fix: file manager

--- fileManager.js
+++ fileManager.js
@@ -4,34 +4,31 @@ const myFilesDirectoryPath = path.join(__dirname, 'myfiles');
 const externalFilesDirectoryPath = path.join(__dirname, 'external');
 
 const readListFiles = () => {
-    // return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       fs.readdirSync(myFilesDirectoryPath, function (err, files) {
         if (err) {
             console.log('Erro ao ler os arquivos: ' + err);
-            // reject(err)
+            reject(err)
         } 
         let filesNames = ''
         files.forEach(function (file) {
             filesNames = filesNames + file + ','
         });
-        // resolve(filesNames)
-        return filesNames
+        resolve(filesNames)
       });
-    // })
+    })
 }
 
 const readFile = async (fileName) => {
-    // return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       fs.readFileSync(myFilesDirectoryPath + '/' + fileName, function (err, file) {
         if (err) {
-          // reject(err);
+          reject(err);
           console.log('Erro ao ler o arquivo: ' + err);
         }
-        // resolve(file.toJSON().data);
-        // resolve(file);
-        return file
+        resolve(file)
       });
-    // });
+    });
 }
 
 const getFileSize = fileName => {
